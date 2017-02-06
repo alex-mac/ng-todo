a2cb55801774e2db726208895bf2a4ba0215e85e
Alex Mac <meet.alexmac@gmail.com>
2017-02-06 00:46:24 -0800

    begin rework to support multiple lists

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -2,13 +2,23 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
   $scope.todos = [];
   $scope.todo = {};
 
+  $scope.lists = [
+    {
+      name: "todos",
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+      name: "groceries",
+      content: "A second list yo."
+    }
+  ]
   $scope.addToList = function() {
     $scope.todos.push({
       text: $scope.todo.text,
       done: false
     });
     $scope.todo.text = '';
-    
+
   }
 
 }]);
