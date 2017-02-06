333b90c4871aec6f53ebbdd133ea877926d3cbe3
Alex Mac <meet.alexmac@gmail.com>
2017-02-06 09:50:29 -0800

    implement adding item to list

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -5,20 +5,22 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
   $scope.lists = [
     {
       name: "todos",
-      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      todoText: "",
+      content: [],
     },
     {
       name: "groceries",
-      content: "A second list yo."
+
+      todoText: "",
+      content: []
     }
   ]
-  $scope.addToList = function() {
-    $scope.todos.push({
-      text: $scope.todo.text,
+  $scope.addToList = function(idx) {
+    $scope.lists[idx].content.push({
+      text: $scope.lists[idx].todoText,
       done: false
     });
-    $scope.todo.text = '';
+    $scope.lists[idx].todoText = '';
 
   }
 
