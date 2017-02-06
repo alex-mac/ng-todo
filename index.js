e0edeb591165dfea0f9d3558d73d14ffc55d75cb
Alex Mac <meet.alexmac@gmail.com>
2017-02-06 14:47:27 -0800

    add simple node server

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname));
+
+app.get('/', (request, response) => {
+  response.render('index')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`Serving up your things to do on port: ${port}`)
+})
